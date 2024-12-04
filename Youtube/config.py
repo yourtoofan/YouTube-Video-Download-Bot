d721e3207551e8caadfa09bfafc48adbcbdb1056
Legend mickey <toofanpapa424@gmail.com>
2024-12-04 13:55:47 +0530

    Update config.py

--- Youtube/config.py
+++ Youtube/config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
      
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
+    API_ID = int(os.environ.get("API_ID", "")
     API_HASH = os.environ.get("API_HASH", "")
     #Add your channel id. For force Subscribe.
     CHANNEL = os.environ.get("CHANNEL", "")
